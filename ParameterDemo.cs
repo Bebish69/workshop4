96935141ed76557e09916c8e0f7a638cf3d5c973
Bebish69 <np05cp4s240120@iic.edu.np>
2025-11-18 11:06:04 +0545

    Add files via upload

---
+++ ParameterDemo.cs
@@ -0,0 +1,22 @@
+﻿namespace Week4_Assignment;
+
+public class ParameterDemo
+{
+    // using REF parameter
+    public void Increase(ref int number)
+    {
+        number += 10;
+    }
+
+    // using OUT parameter
+    public void GetFullName(out string fullname)
+    {
+        fullname = "Bebish Shrestha";  
+    }
+
+    // using PARAMS parameter
+    public int SumAll(params int[] numbers)
+    {
+        return numbers.Sum();
+    }
+}
